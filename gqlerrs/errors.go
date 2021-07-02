8471d74ad42351a6b3f98727da6e28dda38e1ed7
Drahoslav Zan <zandrahoslav@gmail.com>
2021-07-02 12:42:36 +0700

    update

--- gqlerrs/errors.go
+++ gqlerrs/errors.go
@@ -1,13 +1,5 @@
 package gqlerrs
 
-import (
-	"github.com/vektah/gqlparser/v2/gqlerror"
-)
-
-func UnauthorizedError() *gqlerror.Error {
-	return &gqlerror.Error{
-		Extensions: map[string]interface{}{
-			"ecode": "auth/unauthorized",
-		},
-	}
+func UnauthorizedError() SrvError {
+	return MakeSrvError("auth/unauthorized", nil)
 }
---
+++ gqlerrs/srv_error.go
@@ -0,0 +1,53 @@
+package gqlerrs
+
+import (
+	"fmt"
+
+	"github.com/vektah/gqlparser/v2/gqlerror"
+)
+
+type srvError struct {
+	code   string
+	fields map[string]interface{}
+}
+
+type SrvError interface {
+	error
+	Code() string
+	Fields() map[string]interface{}
+	Graph() *gqlerror.Error
+}
+
+func MakeSrvError(code string, fields map[string]interface{}) SrvError {
+	return &srvError{
+		code:   code,
+		fields: fields,
+	}
+}
+
+func (m *srvError) Code() string {
+	return m.code
+}
+
+func (m *srvError) Fields() map[string]interface{} {
+	return m.fields
+}
+
+func (m *srvError) Error() string {
+	if len(m.code) > 0 {
+		if len(m.fields) > 0 {
+			return fmt.Sprintf("%s: %v", m.code, m.fields)
+		}
+		return fmt.Sprintf("%s", m.code)
+	}
+	return fmt.Sprintf("%v", m.fields)
+}
+
+func (m *srvError) Graph() *gqlerror.Error {
+	return &gqlerror.Error{
+		Extensions: map[string]interface{}{
+			"ecode":  m.Code(),
+			"fields": m.Fields(),
+		},
+	}
+}
