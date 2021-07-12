b07a398c4c86c8196b9cfe7251378262aa6dec0c
Drahoslav Zan <zandrahoslav@gmail.com>
2021-07-12 11:10:37 +0700

    dbutils

---
+++ dbutils/marshal.go
@@ -0,0 +1,18 @@
+package dbutils
+
+import "go.mongodb.org/mongo-driver/bson"
+
+func struct2bson(s interface{}) bson.M {
+	data, err := bson.Marshal(s)
+	if err != nil {
+		panic(err)
+	}
+
+	var replace bson.M
+	err = bson.Unmarshal(data, &replace)
+	if err != nil {
+		panic(err)
+	}
+
+	return replace
+}
