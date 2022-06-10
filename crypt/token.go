bca534408555bc03d1da74493cadaf93968570f7
Drahoslav Zan <zandrahoslav@gmail.com>
2022-06-10 23:49:17 +0700

    update

--- crypt/token.go
+++ crypt/token.go
@@ -1,6 +1,8 @@
 package crypt
 
 import (
+	"strings"
+
 	"github.com/google/uuid"
 )
 
@@ -9,4 +11,8 @@ type Token string
 func GenerateToken() Token {
 	guid := uuid.New()
 	return Token(guid.String())
+}
+
+func (m Token) ToID() string {
+	return strings.ReplaceAll(string(m), "-", "")
 }
