75fc53128d9c935795f2d498853c4e4f2644d611
Drahoslav Zan <zandrahoslav@gmail.com>
2022-11-12 00:09:31 +0700

    random

---
+++ crypt/random.go
@@ -0,0 +1,23 @@
+package crypt
+
+import (
+	"math/rand"
+	"time"
+)
+
+var (
+	Alphabet     = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	AlphaNum     = Alphabet + "0123456789"
+	AlphaSpecial = AlphaNum + "~`!@#$%^&*()_-+={[}]|\\:;\"'<,>.?/"
+
+	seededRand = rand.New(rand.NewSource(time.Now().UnixNano()))
+)
+
+func MakeRandom(length int, charset string, prefix string) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset)-1)]
+	}
+
+	return string(b)
+}
