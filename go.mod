9b95a7aef3b78499f544c6d2d0957812eb37f4d0
Drahoslav Zan <zandrahoslav@gmail.com>
2025-02-24 09:42:20 +0700

    cleanup

--- go.mod
+++ go.mod
@@ -3,12 +3,15 @@ module github.com/drahoslavzan/srvutils
 go 1.24
 
 require (
+	github.com/99designs/gqlgen v0.17.66
 	github.com/golang-jwt/jwt/v5 v5.2.1
+	github.com/vektah/gqlparser/v2 v2.5.22
 	go.uber.org/zap v1.27.0
 	golang.org/x/crypto v0.26.0
 )
 
 require (
-	github.com/stretchr/testify v1.9.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/sosodev/duration v1.3.1 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 )
--- gqlerr/errors.go
+++ gqlerr/errors.go
@@ -1,5 +1,13 @@
 package gqlerr
 
+import (
+	"context"
+	"errors"
+
+	"github.com/99designs/gqlgen/graphql"
+	"github.com/vektah/gqlparser/v2/gqlerror"
+)
+
 type Error struct {
 	Code    string
 	Message string
@@ -29,4 +37,23 @@ func NewCodeFieldError(msg, code, field string) *Error {
 
 func (m *Error) Error() string {
 	return m.Message
+}
+
+func (m *Error) Format(ctx context.Context) *gqlerror.Error {
+	e := graphql.DefaultErrorPresenter(ctx, m)
+
+	var ge *Error
+	if errors.As(e, &ge) {
+		e.Message = ge.Message
+		e.Extensions = make(map[string]any)
+
+		if len(ge.Code) > 0 {
+			e.Extensions["code"] = ge.Code
+		}
+		if len(ge.Field) > 0 {
+			e.Extensions["field"] = ge.Field
+		}
+	}
+
+	return e
 }
