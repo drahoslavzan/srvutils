7149cd8a0f574dc9370a87430a98c5495c38c000
Drahoslav Zan <zandrahoslav@gmail.com>
2025-02-25 11:24:23 +0700

    logging

---
+++ logging/context.go
@@ -0,0 +1,47 @@
+package logging
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/google/uuid"
+	"go.uber.org/zap"
+)
+
+type (
+	loggerKey struct{}
+)
+
+func NewLoggerMiddleware(logger *zap.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := Replace(r.Context(), logger)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
+}
+
+func NewTraceLoggerMiddleware(logger *zap.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := Replace(r.Context(), logger.WithLazy(
+				zap.String("requestId", uuid.NewString()),
+			))
+
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
+}
+
+func Replace(ctx context.Context, logger *zap.Logger) context.Context {
+	return context.WithValue(ctx, loggerKey{}, logger)
+}
+
+func MustFromContext(ctx context.Context) *zap.Logger {
+	logger, ok := ctx.Value(loggerKey{}).(*zap.Logger)
+	if !ok {
+		zap.L().Panic("logger instance not provided")
+	}
+
+	return logger
+}
