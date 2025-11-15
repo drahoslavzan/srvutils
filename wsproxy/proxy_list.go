2cac6cb287103a18d423152a889ef511f38cc165
Drahoslav Zan <zandrahoslav@gmail.com>
2025-11-15 07:26:08 +0700

    cleanup

--- wsproxy/proxy_list.go
+++ wsproxy/proxy_list.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"net/http"
+	"net/url"
+	"strconv"
 	"strings"
 )
 
@@ -13,10 +15,53 @@ type (
 	}
 )
 
-func NewProxyList(url string) (*ProxyList, error) {
-	list, err := parseProxies(url)
+func NewProxyList(link string) (*ProxyList, error) {
+	u, err := url.Parse(link)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing link: %w", err)
+	}
+
+	q := u.Query()
+
+	skip := 0
+	qs := q.Get("skip")
+	if qs != "" {
+		var err error
+		skip, err = strconv.Atoi(qs)
+		if err != nil {
+			return nil, fmt.Errorf("invalid skip parameter '%s': %w", qs, err)
+		}
+		if skip < 0 {
+			return nil, fmt.Errorf("skip parameter cannot be negative: %d", skip)
+		}
+	}
+
+	take := 0
+	qt := q.Get("take")
+	if qt != "" {
+		var err error
+		take, err = strconv.Atoi(qt)
+		if err != nil {
+			return nil, fmt.Errorf("invalid take parameter '%s': %w", qt, err)
+		}
+		if take < 1 {
+			return nil, fmt.Errorf("take parameter must be positive: %d", take)
+		}
+	}
+
+	list, err := parseProxies(link)
 	if err != nil {
 		return nil, err
+	}
+
+	if skip > 0 {
+		if skip >= len(list) {
+			return nil, fmt.Errorf("skipping %d proxies out of %d", skip, len(list))
+		}
+		list = list[skip:]
+	}
+	if take > 0 {
+		list = list[:min(len(list), take)]
 	}
 
 	ret := &ProxyList{
@@ -28,11 +73,6 @@ func NewProxyList(url string) (*ProxyList, error) {
 
 func (m *ProxyList) List() []string {
 	return m.list
-}
-
-func (m *ProxyList) TruncateTo(v int) {
-	cnt := max(0, min(v, len(m.list)))
-	m.list = m.list[:cnt]
 }
 
 func (m *ProxyList) Clients(cfg *Config) (*ProxyClients, error) {
